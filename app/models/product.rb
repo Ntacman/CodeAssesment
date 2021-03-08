5510d52fe06278b1846ab5c603222fc51833807f
Ntacman <Ntacman@users.noreply.github.com>
2021-03-08 13:22:14 -0600

    add presence validations for product fields

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :orders, :foreign_key => :product_id
   has_many :inventories, :foreign_key => :product_id
+  validates :product_name, :presence => true
+  validates :style, :presence => true
+  validates :brand, :presence => true
+  validates :shipping_price, :presence => true
+  validates :admin_id, :presence => true
 end
